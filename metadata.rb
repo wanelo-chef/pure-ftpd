71de37636d1860c615d83d9ef1685de8a32c145e
Eric Saxby & Greg Poirier <eric+greg@wanelo.com>
2014-12-04 17:59:33 -0800

    0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@wanelo.com'
 license 'MIT'
 description 'Installs and configures pure-ftp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.2.1'
 
 depends 'build-essential'
 depends 'paths'
