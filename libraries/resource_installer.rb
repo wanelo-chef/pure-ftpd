48fbd6aa3d66461f458593314ccfaf185e627fa4
Eric Saxby & Greg Poirier <eric+greg@wanelo.com>
2014-12-04 17:45:50 -0800

    Align values to hush rubocop

--- libraries/resource_installer.rb
+++ libraries/resource_installer.rb
@@ -36,21 +36,21 @@ class Chef
 
       def cflags
         %w(-DUPLOAD_PIPE_FILE='"/var/run/pure-ftpd/pure-ftpd.upload.pipe"'
-        -DUPLOAD_PIPE_LOCK='"/var/run/pure-ftpd/pure-ftpd.upload.lock"'
-        -O2 -pipe -O2 -DLDAP_DEPRECATED -I/opt/local/include
-        -I/usr/include -I/opt/local/include/mysql
-        -I/opt/local/include/gettext).join(' ')
+           -DUPLOAD_PIPE_LOCK='"/var/run/pure-ftpd/pure-ftpd.upload.lock"'
+           -O2 -pipe -O2 -DLDAP_DEPRECATED -I/opt/local/include
+           -I/usr/include -I/opt/local/include/mysql
+           -I/opt/local/include/gettext).join(' ')
       end
 
       def ldflags
         %w(-L/opt/local/gcc47/lib/gcc/x86_64-sun-solaris2.11/4.7.3
-         -Wl,-R/opt/local/gcc47/lib/gcc/x86_64-sun-solaris2.11/4.7.3
-         -L/opt/local/gcc47/lib
-         -Wl,-R/opt/local/gcc47/lib
-         -L/opt/local/lib
-         -Wl,-R/opt/local/lib
-         -L/usr/lib/amd64
-         -Wl,-R/usr/lib/amd64).join(' ')
+           -Wl,-R/opt/local/gcc47/lib/gcc/x86_64-sun-solaris2.11/4.7.3
+           -L/opt/local/gcc47/lib
+           -Wl,-R/opt/local/gcc47/lib
+           -L/opt/local/lib
+           -Wl,-R/opt/local/lib
+           -L/usr/lib/amd64
+           -Wl,-R/usr/lib/amd64).join(' ')
       end
     end
   end
